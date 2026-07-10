f6dfaa9fc5f8aab784de50de004410e27b72d609
Hamza Kothari <hamzakothari1978@gmail.com>
2026-07-10 19:40:58 +0530

    Add solution: Score of a String

---
+++ score-of-a-string/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        add = 0
+        for i in range(len(s)-1):
+            add+= abs(ord(s[i])-ord(s[i+1]))
+        return add
+
+
