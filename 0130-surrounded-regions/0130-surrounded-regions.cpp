8a867393e03bfa932720742d0aa4bc5af269d4ec
Hamza Kothari <hamzakothari1978@gmail.com>
2026-07-30 19:41:37 +0530

    Time: 5 ms (7.86%), Space: 14.6 MB (49.28%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -8,24 +8,48 @@ public:
         queue<pair<int,int>> q;
        
         for(int i = 0 ; i<m ; i++){
-            if(grid[0][i] == 'O')  q.push({0,i});
-            if(grid[n-1][i] == 'O') q.push({n-1,i}); 
+            if(grid[0][i] == 'O') {
+                q.push({0,i});
+                grid[0][i]  = '#';
+                }
+            if(grid[n-1][i] == 'O'){
+                q.push({n-1,i});
+                grid[n-1][i] = '#';
+                } 
         }
 
         for(int i = 0 ; i<n ; i++){
-            if(grid[i][0] == 'O') q.push({i,0});
-            if(grid[i][m-1] == 'O') q.push({i,m-1}); 
+            if(grid[i][0] == 'O'){
+                q.push({i,0});
+                grid[i][0] = '#';
+                }
+            if(grid[i][m-1] == 'O'){
+                q.push({i,m-1});
+                grid[i][m-1] = '#';
+                } 
         }
         while(!q.empty()){
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
-            grid[row][col] = '#';
             
-            if(row>0 && grid[row-1][col] == 'O') q.push({row-1,col}); // UP
-            if(row<n-1 && grid[row+1][col] == 'O') q.push({row+1,col});  // DOWN
-            if(col>0 && grid[row][col-1] == 'O') q.push({row,col-1}); // LEFT
-            if(col<m-1 && grid[row][col+1] == 'O') q.push({row,col+1}); // RIGHT
+            
+            if(row>0 && grid[row-1][col] == 'O'){
+                q.push({row-1,col}); // UP
+                grid[row-1][col] = '#';
+            }
+            if(row<n-1 && grid[row+1][col] == 'O'){
+                q.push({row+1,col});  // DOWN
+                grid[row+1][col] = '#';
+                }
+            if(col>0 && grid[row][col-1] == 'O'){
+                q.push({row,col-1}); // LEFT
+                grid[row][col-1] = '#';
+                }
+            if(col<m-1 && grid[row][col+1] == 'O'){
+                q.push({row,col+1}); // RIGHT
+                grid[row][col+1] = '#';
+                }
         }
         for(int i = 0 ; i<n ; i++){
             for(int j = 0 ; j<m ; j++){
