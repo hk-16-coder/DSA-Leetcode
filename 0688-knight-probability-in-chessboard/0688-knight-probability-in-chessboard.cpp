b741eef1235df0bf763ce598c91246ebdda9ad32
Hamza Kothari <hamzakothari1978@gmail.com>
2026-09-01 17:03:02 +0530

    Time: 347 ms (5.07%), Space: 92.7 MB (5.14%) - LeetHub

---
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double f(int moves , int row , int col , int n , vector<vector<vector<double>>>& dp){
+        if(row<0 || row>=n || col<0 || col>=n) return 0;
+        if(moves == 0) return 1;
+        
+        if(dp[moves][row][col] != -1) return dp[moves][row][col]; 
+        vector<vector<int>> move = {{-2,-1} , {-2,1} , {2,-1} , {2,1} , {-1,-2} , {-1,2} , {1,-2} , {1,2}};
+        
+        double prob = 0;
+        for(int i = 0 ; i<8 ; i++){
+            int newR = row +  move[i][0];
+            int newC = col + move[i][1];
+
+            prob += f(moves-1 , newR , newC , n,dp);
+        }
+        return dp[moves][row][col] =  prob/8;
+    }
+    double knightProbability(int n, int k, int row, int column) {
+        vector<vector<vector<double>>> dp(k+1 , vector<vector<double>>(n,vector<double>(n,-1)));
+        return f(k,row,column,n,dp);
+    }
+};
