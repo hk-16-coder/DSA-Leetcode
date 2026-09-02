2e052e4c5e8c5fc60fde23588c2a35951816eb20
Hamza Kothari <hamzakothari1978@gmail.com>
2026-09-02 16:22:01 +0530

    Time: 0 ms (100%), Space: 30.5 MB (7.23%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // if all elements are odd or even then nothingto think ,
+        // but when there are both odd and even elements present we can convert any even to odd by just substracting
+        // even - odd = odd , whereeas odd - even = odd 
+    
+        return true;
+    }
+};
