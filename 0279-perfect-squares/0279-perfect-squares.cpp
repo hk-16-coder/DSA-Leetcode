f98f69cf3ccb9b3a555836789bf19211b1ec7227
Hamza Kothari <hamzakothari1978@gmail.com>
2026-09-07 20:52:25 +0530

    Time: 107 ms (26.86%), Space: 15.6 MB (20.74%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int numSquares(int n) {
         int nums = sqrt(n);
-        vector<vector<int>> dp(nums + 1, vector<int>(n+1));
+
+        vector<int> curr(n+1) , prev(n+1);
     
         for(int i = 1 ; i<=n ; i++){
-            dp[0][i] = 1e9;
+            prev[i] = 1e9;
         }
 
         for(int idx = 1 ; idx<=nums ; idx++){
             for(int T = 1 ; T<=n ; T++){
-                int notTake = dp[idx-1][T];
+                int notTake = prev[T];
                 int take = 1e9;
                 if(idx*idx <= T){
-                     take = 1 + dp[idx][T - idx*idx];
+                     take = 1 + curr[T - idx*idx];
                 }
 
-               dp[idx][T] = min(take,notTake);
+               curr[T] = min(take,notTake);
             }
+            prev = curr;
         }
-        return dp[nums][n];
+        return prev[n];
     }
 };
