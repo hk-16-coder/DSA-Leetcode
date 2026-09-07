26c2935107dc0902501d1065563eb4b21874e5c7
Hamza Kothari <hamzakothari1978@gmail.com>
2026-09-07 20:48:24 +0530

    Time: 183 ms (18.12%), Space: 162.4 MB (10.12%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -4,7 +4,7 @@ public:
         if(idx == 1){
             return n;
         }
-        
+
         if(dp[idx][n] != -1) return dp[idx][n];
 
         int notTake = f(idx-1 , n , dp);
@@ -15,9 +15,26 @@ public:
 
         return dp[idx][n] = min(take,notTake);
     }
+
     int numSquares(int n) {
-        int idx = sqrt(n);
-        vector<vector<int>> dp(idx + 1, vector<int>(n+1,-1));
-        return f(idx,n,dp); 
+        int nums = sqrt(n);
+        vector<vector<int>> dp(nums + 1, vector<int>(n+1));
+    
+        for(int i = 1 ; i<=n ; i++){
+            dp[0][i] = 1e9;
+        }
+
+        for(int idx = 1 ; idx<=nums ; idx++){
+            for(int T = 1 ; T<=n ; T++){
+                int notTake = dp[idx-1][T];
+                int take = 1e9;
+                if(idx*idx <= T){
+                     take = 1 + dp[idx][T - idx*idx];
+                }
+
+               dp[idx][T] = min(take,notTake);
+            }
+        }
+        return dp[nums][n];
     }
 };
