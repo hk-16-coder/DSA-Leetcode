fd907c33b9203bb127b3c479727a5bbc877ecf69
Hamza Kothari <hamzakothari1978@gmail.com>
2026-09-10 18:16:56 +0530

    Time: 394 ms (48.2%), Space: 211.6 MB (39.6%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
-    int f(int idx , int buy , int trans , vector<int>&prices , vector<vector<vector<int>>>& dp){
-        if(idx == prices.size()) return 0;
-        if(trans == 2) return 0;
-        if(dp[idx][buy][trans] != -1) return dp[idx][buy][trans];
+    // int f(int idx , int buy , int trans , vector<int>&prices , vector<vector<vector<int>>>& dp){
+    //     if(idx == prices.size()) return 0;
+    //     if(trans == 2) return 0;
+    //     if(dp[idx][buy][trans] != -1) return dp[idx][buy][trans];
         
-        if(buy){
-            int buy0 = -prices[idx] + f(idx+1 , 0 , trans , prices, dp);
-            int dontBuy = f(idx+1 , 1 , trans , prices , dp);
-            return dp[idx][buy][trans] = max(buy0 , dontBuy);
-        }
+    //     if(buy){
+    //         int buy0 = -prices[idx] + f(idx+1 , 0 , trans , prices, dp);
+    //         int dontBuy = f(idx+1 , 1 , trans , prices , dp);
+    //         return dp[idx][buy][trans] = max(buy0 , dontBuy);
+    //     }
 
-        else{
-            int sell = prices[idx] + f(idx+1 , 1 , trans+1 , prices , dp);
-            int dontSell = f(idx+1 , 0 , trans , prices , dp);
-            return dp[idx][buy][trans] = max(sell , dontSell);
-        }
-    }
+    //     else{
+    //         int sell = prices[idx] + f(idx+1 , 1 , trans+1 , prices , dp);
+    //         int dontSell = f(idx+1 , 0 , trans , prices , dp);
+    //         return dp[idx][buy][trans] = max(sell , dontSell);
+    //     }
+    // }
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1 , vector<vector<int>> ( 2 , vector<int>(2,-1)));
-        return f(0 , 1 , 0 , prices , dp);
+        vector<vector<vector<int>>> dp(n+1 , vector<vector<int>> ( 2 , vector<int>(3)));
+        //return f(0 , 1 , 0 , prices , dp);
+
+        for(int idx = n-1 ; idx>=0 ; idx--){
+            for(int buy = 0 ; buy<=1 ; buy++){
+                for(int trans = 0 ; trans<=1 ; trans++){
+                    long long profit = 0;
+                    if(buy){
+                        profit = max(-prices[idx] + dp[idx+1][0][trans] , dp[idx+1][1][trans]);
+                    }
+
+                    else{
+                        profit = max(prices[idx] + dp[idx+1][1][trans+1] , dp[idx+1][0][trans]);
+                    }
+                    dp[idx][buy][trans] = profit;
+                }
+            }
+        }
+        return dp[0][1][0];
     }
 };
